1f582e3637924fdb109f2689a3b0751a0017af58
bruhGrassi <brunagm88@gmail.com>
2025-02-24 19:23:55 -0300

    chore: add action

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import InfiniteScroll from './components/InfiniteScroll';
-import Pagination from './components/Pagination';
-import './App.css';
-
+import InfiniteScroll from "./components/InfiniteScroll";
+import Pagination from "./components/Pagination";
+import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [pagination, setPagination] = useState("pagination");
+
   return (
     <div className="container">
       <h1>Personagens Disney</h1>
-      {/*<InfiniteScroll />*/}
-      <Pagination/>
+
+      <div className="action">
+        <button onClick={() => setPagination("pagination")}>Paginação</button>
+        <button onClick={() => setPagination("scroll")}>Scroll Infinito</button>
+      </div>
+      {pagination !== "pagination" && <InfiniteScroll />}
+      {pagination === "pagination" && <Pagination />}
     </div>
   );
-}
+};
 
 export default App;
